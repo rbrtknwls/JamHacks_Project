e3f56fc53d4b5a0f178be9372b2419ccce9a4b94
yodasnow <oliverode@gmail.com>
2018-05-06 03:17:24 -0400

    Minor changes

---
+++ algorithm.js
@@ -0,0 +1,12 @@
+//Returns amount of sleep you had
+function sleepFinder(){
+    return(-1)
+}
+//Returns amount of studying
+function studyFinder(){
+    return(-1)
+}
+//Optimal mark calculator
+function markCalculator(){
+    return(-1)
+}
